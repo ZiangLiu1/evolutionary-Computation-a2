6cca1100f2105d8a0afa0c6db0100bdd95e4f28f
ZiangLiu1 <140679348+ZiangLiu1@users.noreply.github.com>
2024-10-09 02:10:05 +1030

    Update pso_lib.py

--- pso_lib.py
+++ pso_lib.py
@@ -1,34 +1,50 @@
-# pso_lib.py
+# pso_lib.py (continued)
 
-import numpy as np
+class Particle:
+    def __init__(self, dimensions, problem):
+        self.position = np.random.uniform(problem.lower_bound, problem.upper_bound, dimensions)
+        self.velocity = np.random.uniform(-1, 1, dimensions)
+        self.best_position = self.position.copy()
+        self.best_fitness = float('inf')
 
-class RealValuedOptimizationProblem:
-    def __init__(self, dimensions, lower_bound, upper_bound):
-        self.dimensions = dimensions
-        self.lower_bound = lower_bound
-        self.upper_bound = upper_bound
+    def update_velocity(self, inertia, cognitive_component, social_component, global_best_position):
+        cognitive_velocity = cognitive_component * np.random.random() * (self.best_position - self.position)
+        social_velocity = social_component * np.random.random() * (global_best_position - self.position)
+        self.velocity = inertia * self.velocity + cognitive_velocity + social_velocity
 
-    def evaluate(self, position):
-        """
-        Abstract method to be overridden by specific problem implementations.
-        """
-        raise NotImplementedError("Subclasses should implement this method.")
+    def move(self):
+        self.position += self.velocity
 
-class AckleyFunction(RealValuedOptimizationProblem):
-    def __init__(self, dimensions, lower_bound=-30, upper_bound=30):
-        super().__init__(dimensions, lower_bound, upper_bound)
+class PSO:
+    def __init__(self, problem, swarm_size=30, inertia=0.7, cognitive_component=1.5, social_component=1.5, iterations=1000):
+        self.problem = problem
+        self.swarm_size = swarm_size
+        self.inertia = inertia
+        self.cognitive_component = cognitive_component
+        self.social_component = social_component
+        self.iterations = iterations
+        self.swarm = [Particle(problem.dimensions, problem) for _ in range(swarm_size)]
+        self.global_best_position = None
+        self.global_best_fitness = float('inf')
 
-    def evaluate(self, position):
-        """
-        Evaluate the Ackley function.
-        """
-        a = 20
-        b = 0.2
-        c = 2 * np.pi
-        d = len(position)
-        
-        sum_sq_term = -a * np.exp(-b * np.sqrt(np.sum(np.square(position)) / d))
-        cos_term = -np.exp(np.sum(np.cos(c * position)) / d)
-        result = a + np.exp(1) + sum_sq_term + cos_term
-        
-        return result
+    def optimize(self):
+        for iteration in range(self.iterations):
+            for particle in self.swarm:
+                fitness = self.problem.evaluate(particle.position)
+                
+                # Update personal best
+                if fitness < particle.best_fitness:
+                    particle.best_fitness = fitness
+                    particle.best_position = particle.position.copy()
+
+                # Update global best
+                if fitness < self.global_best_fitness:
+                    self.global_best_fitness = fitness
+                    self.global_best_position = particle.position.copy()
+
+            # Update velocities and move particles
+            for particle in self.swarm:
+                particle.update_velocity(self.inertia, self.cognitive_component, self.social_component, self.global_best_position)
+                particle.move()
+
+        return self.global_best_position, self.global_best_fitness
